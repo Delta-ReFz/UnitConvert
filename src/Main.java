b9745822ec207e9ae0d0a4ff72c4a92ec19c1733
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-11-01 23:57:47 -0400

    Interface User
    Trying different logic, design of the interface user

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
-        int age = 20;
+        String msg1 = "Choose your physical quantities:";
+        System.out.println(msg1);
 
-        System.out.println(age);
+        Scanner s = new Scanner(System.in);
+
+        String resp1 = s.nextLine();
+
+        if(resp1.equals("length")) {
+            System.out.println("you have chosen the length, decide which units you want to convert to: ");
+
+            String resp2 = s.nextLine();
+
+            if(resp2.equals("m")) {
+
+                System.out.println("Chose the unit you want to convert your meters into: ");
+
+                int resp3 = s.nextInt();
+
+                if(resp3 >= 0) {
+
+                    System.out.println("Decide which units you want to convert to:");
+
+                    String resp4 = s.nextLine();
+
+                    if(resp4.equals("km")) {
+
+                        resp3 = resp3 /1000;
+
+                        System.out.println(resp3 + "km");
+
+                    }
+                }
+
+
+            }
+
+        }
+
+        else {
+            System.out.println("you chose something else");
+        }
     }
 }
