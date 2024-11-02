1863a3c8e7e92835749a44c9b3bd549c7e8d6628
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-11-02 02:01:59 -0400

    Added a new file for the algos

---
+++ src/Algorythmes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Algorythmes {
+
+    double resp3;
+    Affichage reponse = new Affichage();
+    Scanner s = new Scanner(System.in);
+
+    public void conversion() {
+
+        if(reponse.resp1.equals("length")) {
+            System.out.println("You have chosen the length, decide which units you want to convert (km,m,cm,mm)");
+
+            String resp2 = s.nextLine();
+
+            if(resp2.equals("m")) {
+
+                System.out.println("Enter the number of meters you want to convert:");
+
+                resp3 = s.nextInt();
+                s.nextLine();
+
+                if(resp3 >= 0) {
+
+                    System.out.println("Decide which units you want to convert your meters to (km,m,cm,mm)");
+
+                    String resp4 = s.nextLine();
+
+                    if(resp4.equals("km")) {
+
+                        resp3 = resp3 /1000;
+
+                        System.out.println(resp3 + "km");
+                    }
+                }
+            }
+        }
+
+        else {
+            System.out.println("you chose something else");
+        }
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,16 @@ public class Main {
 
         Affichage affichage = new Affichage();
         Affichage reponse = new Affichage();
+        Algorythmes convert = new Algorythmes();
 
         Scanner s = new Scanner(System.in);
-
-        double resp3 = 0;
 
         affichage.affichage1();
         reponse.reponse1();
 
-        if(reponse.resp1.equals("length")) {
-            System.out.println("You have chosen the length, decide which units you want to convert (km,m,cm,mm)");
+        convert.conversion();
 
-            String resp2 = s.nextLine();
-
-            if(resp2.equals("m")) {
-
-                System.out.println("Enter the number of meters you want to convert:");
-
-                resp3 = s.nextInt();
-                s.nextLine();
-
-                if(resp3 >= 0) {
-
-                    System.out.println("Decide which units you want to convert your meters to (km,m,cm,mm)");
-
-                    String resp4 = s.nextLine();
-
-                    if(resp4.equals("km")) {
-
-                        resp3 = resp3 /1000;
-
-                        System.out.println(resp3 + "km");
-                    }
-                }
-            }
-        }
-
-        else {
-            System.out.println("you chose something else");
-        }
     }
+
+
 }
